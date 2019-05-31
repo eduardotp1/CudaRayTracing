909df632de16b9f3e00e5c1ff31ce1fa38deeeca
eduardotp1 <edu.tirta@gmail.com>
2019-05-31 09:14:41 -0300

    vec3

--- raytracinginoneweekend/main.cu
+++ raytracinginoneweekend/main.cu
@@ -1,26 +1,34 @@
 #include <iostream>
 #include <time.h>
+#include "vec3.h"
 
-__global__ void div(float *buffer, int max_x, int max_y) {
+__global__ void render(vec3 *buffer, int max_x, int max_y) {
     int i = threadIdx.x + blockIdx.x * blockDim.x;
     int j = threadIdx.y + blockIdx.y * blockDim.y;
     if((i >= max_x) || (j >= max_y)) return;
-    int pixel_index = j*max_x*3 + i*3;
-    buffer[pixel_index + 0] = float(i) / max_x;//r
-    buffer[pixel_index + 1] = float(j) / max_y;//g
-    buffer[pixel_index + 2] = 0.2;//b
+    int pixel_index = j*max_x + i;
+    buffer[pixel_index] = vec3( float(i) / max_x, float(j) / max_y, 0.2f);
 }
 
 int main() {
     int nx = 1200;
     int ny = 600;
-    int num_pixels = nx*ny;
-    size_t buffer_size = 3*num_pixels*sizeof(float);
-    float *buffer;
 
+    int num_pixels = nx*ny;
+    size_t buffer_size = 3*num_pixels*sizeof(vec3);
+    vec3 *buffer;
+    cudaMallocManaged((void **)&buffer, buffer_size)
+
+    clock_t start, stop;
+    start = clock();
     dim3 block_size(nx/9,ny/9);//tamanho de cada bloco
     dim3 size_grid(8,8);//divisao da imagem
     div<<<size_grid, block_size>>>(buffer, nx, ny);//chama a gpu
+    cudaDeviceSynchronize()
+
+    stop = clock();
+    double timer_seconds = ((double)(stop - start)) / CLOCKS_PER_SEC;
+    std::cerr << "time:" << timer_seconds;
 
     
     std::cout << "P3\n" << nx << " " << ny << "\n255\n";
