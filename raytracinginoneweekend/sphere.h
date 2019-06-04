32b7befd852dbc945386f7a02190b3a0a5d10bde
eduardotp1 <edu.tirta@gmail.com>
2019-06-04 14:06:17 -0300

    sphere2

--- raytracinginoneweekend/sphere.h
+++ raytracinginoneweekend/sphere.h
@@ -17,7 +17,8 @@
 class sphere: public hitable  {
     public:
         __device__ sphere() {}
-        __device__ sphere(vec3 cen, float r, material *m) : center(cen), radius(r), mat_ptr(m)  {};
+        // __device__ sphere(vec3 cen, float r, material *m) : center(cen), radius(r), mat_ptr(m)  {};
+        __device__ sphere(vec3 cen, float r) : center(cen), radius(r)  {};
         __device__ virtual bool hit(const ray& r, float tmin, float tmax, hit_record& rec) const;
         vec3 center;
         float radius;
