05f6eee3701125143f35c6676e60a6b9cc8af699
eduardotp1 <edu.tirta@gmail.com>
2019-06-04 10:08:17 -0300

    ch1

--- raytracinginoneweekend/main.cu
+++ raytracinginoneweekend/main.cu
@@ -1,102 +1,135 @@
+//==================================================================================================
+// Written in 2016 by Peter Shirley <ptrshrl@gmail.com>
+//
+// To the extent possible under law, the author(s) have dedicated all copyright and related and
+// neighboring rights to this software to the public domain worldwide. This software is distributed
+// without any warranty.
+//
+// You should have received a copy (see file COPYING.txt) of the CC0 Public Domain Dedication along
+// with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+// This code was change following the tutorial of Roger Allen https://devblogs.nvidia.com/accelerated-ray-tracing-cuda/
+//==================================================================================================
+
 #include <iostream>
-#include <time.h>
-#include <float.h>
-#include "vec3.h"
-#include "ray.h"
 #include "sphere.h"
 #include "hitable_list.h"
+#include "float.h"
+#include "camera.h"
+#include "material.h"
 
 
-
-__device__ vec3 color(const ray& r, hitable **world) {
+vec3 color(const ray& r, hitable *world, int depth) {
     hit_record rec;
-    if ((*world)->hit(r, 0.0, FLT_MAX, rec)) {
-        return 0.5f*vec3(rec.normal.x()+1.0f, rec.normal.y()+1.0f, rec.normal.z()+1.0f);
+    if (world->hit(r, 0.001, MAXFLOAT, rec)) { 
+        ray scattered;
+        vec3 attenuation;
+        if (depth < 50 && rec.mat_ptr->scatter(r, rec, attenuation, scattered)) {
+             return attenuation*color(scattered, world, depth+1);
+        }
+        else {
+            return vec3(0,0,0);
+        }
     }
     else {
         vec3 unit_direction = unit_vector(r.direction());
-        float t = 0.5f*(unit_direction.y() + 1.0f);
-        return (1.0f-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
+        float t = 0.5*(unit_direction.y() + 1.0);
+        return (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
     }
 }
 
-__global__ void div(vec3 *buffer, int max_x, int max_y,
-                       vec3 lower_left_corner, vec3 horizontal, vec3 vertical, vec3 origin,
-                       hitable **world) {
+
+hitable *random_scene() {
+    int n = 500;
+    hitable **list = new hitable*[n+1];
+    list[0] =  new sphere(vec3(0,-1000,0), 1000, new lambertian(vec3(0.5, 0.5, 0.5)));
+    int i = 1;
+    for (int a = -11; a < 11; a++) {
+        for (int b = -11; b < 11; b++) {
+            float choose_mat = drand48();
+            vec3 center(a+0.9*drand48(),0.2,b+0.9*drand48()); 
+            if ((center-vec3(4,0.2,0)).length() > 0.9) { 
+                if (choose_mat < 0.8) {  // diffuse
+                    list[i++] = new sphere(center, 0.2, new lambertian(vec3(drand48()*drand48(), drand48()*drand48(), drand48()*drand48())));
+                }
+                else if (choose_mat < 0.95) { // metal
+                    list[i++] = new sphere(center, 0.2,
+                            new metal(vec3(0.5*(1 + drand48()), 0.5*(1 + drand48()), 0.5*(1 + drand48())),  0.5*drand48()));
+                }
+                else {  // glass
+                    list[i++] = new sphere(center, 0.2, new dielectric(1.5));
+                }
+            }
+        }
+    }
+
+    list[i++] = new sphere(vec3(0, 1, 0), 1.0, new dielectric(1.5));
+    list[i++] = new sphere(vec3(-4, 1, 0), 1.0, new lambertian(vec3(0.4, 0.2, 0.1)));
+    list[i++] = new sphere(vec3(4, 1, 0), 1.0, new metal(vec3(0.7, 0.6, 0.5), 0.0));
+
+    return new hitable_list(list,i);
+}
+
+__global__ void render(float *fb, int max_x, int max_y) {
     int i = threadIdx.x + blockIdx.x * blockDim.x;
     int j = threadIdx.y + blockIdx.y * blockDim.y;
     if((i >= max_x) || (j >= max_y)) return;
-    int pixel_index = j*max_x + i;
-    float u = float(i) / float(max_x);
-    float v = float(j) / float(max_y);
-    ray r(origin, lower_left_corner + u*horizontal + v*vertical);
-    buffer[pixel_index] = color(r, world);
-}
-
-__global__ void create_world(hitable **d_list, hitable **d_world) {
-    if (threadIdx.x == 0 && blockIdx.x == 0) {
-        *(d_list)   = new sphere(vec3(0,0,-1), 0.5);
-        *(d_list+1) = new sphere(vec3(0,-100.5,-1), 100);
-        *d_world    = new hitable_list(d_list,2);
-    }
-}
-
-__global__ void free_world(hitable **d_list, hitable **d_world) {
-    delete *(d_list);
-    delete *(d_list+1);
-    delete *d_world;
+    int pixel_index = j*max_x*3 + i*3;
+    fb[pixel_index + 0] = float(i) / max_x;
+    fb[pixel_index + 1] = float(j) / max_y;
+    fb[pixel_index + 2] = 0.2;
 }
 
 int main() {
     int nx = 1200;
-    int ny = 600;
+    int ny = 800;
+    int ns = 10;
+    int tx = 8;//divisoes que vai ser cortada a imagem
+    int ty = 8;//divisoes que vai ser cortada a imagem
     int num_pixels = nx*ny;
-    size_t buffer_size = num_pixels*sizeof(vec3);
+    size_t fb_size = 3*num_pixels*sizeof(float);
 
-    vec3 *buffer;
-    cudaMallocManaged((void **)&buffer, buffer_size);
+    // allocate FB
+    float *fb;
+    cudaMallocManaged((void **)&fb, fb_size);
 
-    hitable **d_list;
-    cudaMalloc((void **)&d_list, 2*sizeof(hitable *));
-    hitable **d_world;
-    cudaMalloc((void **)&d_world, sizeof(hitable *)));
-    create_world<<<1,1>>>(d_list,d_world);
+    dim3 block_size(nx/tx+1,ny/ty+1);//tamanho de cada grid
+    dim3 size_grid(tx,ty);//tamanho do grid
+    render<<<block_size, size_grid>>>(fb, nx, ny);//manda para a GPU calcular
     cudaGetLastError();
     cudaDeviceSynchronize();
-
-    clock_t start, stop;
-    start = clock();
-    
-    dim3 block_size(nx/9,ny/9);
-    dim3 size_grid(8,8);
-    div<<<block_size, size_grid>>>(buffer, nx, ny,
-                                vec3(-2.0, -1.0, -1.0),
-                                vec3(4.0, 0.0, 0.0),
-                                vec3(0.0, 2.0, 0.0),
-                                vec3(0.0, 0.0, 0.0),
-                                d_world);
-    cudaGetLastError();
-    cudaDeviceSynchronize();
-    stop = clock();
-    double timer_seconds = ((double)(stop - start)) / CLOCKS_PER_SEC;
-    std::cerr << "time:" << timer_seconds;
 
     std::cout << "P3\n" << nx << " " << ny << "\n255\n";
+    // hitable *list[5];
+    float R = cos(M_PI/4);
+    list[0] = new sphere(vec3(0,0,-1), 0.5, new lambertian(vec3(0.1, 0.2, 0.5)));
+    list[1] = new sphere(vec3(0,-100.5,-1), 100, new lambertian(vec3(0.8, 0.8, 0.0)));
+    list[2] = new sphere(vec3(1,0,-1), 0.5, new metal(vec3(0.8, 0.6, 0.2), 0.0));
+    list[3] = new sphere(vec3(-1,0,-1), 0.5, new dielectric(1.5));
+    list[4] = new sphere(vec3(-1,0,-1), -0.45, new dielectric(1.5));
+    hitable *world = new hitable_list(list,5);
+    world = random_scene();
+
+    vec3 lookfrom(13,2,3);
+    vec3 lookat(0,0,0);
+    float dist_to_focus = 10.0;
+    float aperture = 0.1;
+
+    camera cam(lookfrom, lookat, vec3(0,1,0), 20, float(nx)/float(ny), aperture, dist_to_focus);
+
     for (int j = ny-1; j >= 0; j--) {
         for (int i = 0; i < nx; i++) {
-            size_t pixel_index = j*nx + i;
-            int ir = int(255.99*buffer[pixel_index].r());
-            int ig = int(255.99*buffer[pixel_index].g());
-            int ib = int(255.99*buffer[pixel_index].b());
+            size_t pixel_index = j*3*nx + i*3;
+            float r = fb[pixel_index + 0];
+            float g = fb[pixel_index + 1];
+            float b = fb[pixel_index + 2];
+            int ir = int(255.99*r);
+            int ig = int(255.99*g);
+            int ib = int(255.99*b);
             std::cout << ir << " " << ig << " " << ib << "\n";
         }
     }
-
-    cudaDeviceSynchronize();
-    free_world<<<1,1>>>(d_list,d_world);
-    cudaGetLastError();
-    cudaFree(d_list);
-    cudaFree(d_world);
-    cudaFree(buffer);
-    cudaDeviceReset();
+    checkCudaErrors(cudaFree(fb));
 }
+
+
+
